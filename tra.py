97909bff46bffaa26ac36e76b9e9c372ad51a535
slavikyd <47947352+slavikyd@users.noreply.github.com>
2021-10-23 23:08:43 +0300

    Update tra.py

--- tra.py
+++ tra.py
@@ -1,8 +1,24 @@
 import math
-#скрипт для перевода координат из TLE в готовые координаты дрона
+#скрипт для перевода координат из TLE в готовые координаты дрона 
 x = float(input('X - '))
 x1 = float(input('X1 - '))
 y = float(input('Y - '))
 y1 = float(input('Y1 - '))
 
 print(math.sqrt((x-x1)**2+(y-y1)**2))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#formula by @Li0neI
